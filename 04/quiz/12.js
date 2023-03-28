fa1076b76e68258a40d9fb8522ad288189b8e968
migyeong <rmfosem613@gmail.com>
2023-03-28 23:43:54 +0900

    quiz

--- 04/quiz/12.js
+++ 04/quiz/12.js
@@ -8,14 +8,11 @@ const pageSize = 3;
 // pageSize에 따라 배열값 분리해서 출력
 
 let arr = [];
-let result = [];
 
 for (let i = 0; i < items.length; i += pageSize) {
   arr.push(items.slice(i, i + pageSize));
 }
 
-for (let i = 0; i < arr.length; i++) {
-  arr[i] = arr[i].join(",");
-}
+let result = JSON.stringify(arr).split(/"/).join("");
 // 출력: [ [a,b,c], [d,e,f], [g,h,i], [j] ]
-console.log(arr);
+console.log(result);
