637bb9cd6e030e7c087ab594aa7f15f82c2e10ca
migyeong <rmfosem613@gmail.com>
2023-04-19 23:45:12 +0900

    07

--- 06/quiz5.js
+++ 06/quiz5.js
@@ -1,51 +1,27 @@
 let x = 0;
 let y = 0;
-let offset = { x: 0, y: 0 };
 let isDown = false;
+let offset = { x: 0, y: 0 };
 let $dragEl = null;
 
-const startDrag = (event) => {
-  console.log("mousedown");
-  // console.log(event.clientX, event.clientY);
-  // console.log(x, y);
-
-  $dragEl = event.target;
-
-  offset.x = $dragEl.offsetLeft - event.clientX;
-  offset.y = $dragEl.offsetTop - event.clientY;
+document.body.addEventListener("mousedown", (e) => {
   isDown = true;
-};
+  $dragEl = e.target;
 
-const stopDrag = (event) => {
-  // console.log('mouseup');
+  offset.x = $dragEl.offsetLeft - e.clientX;
+  offset.y = $dragEl.offsetTop - e.clientY;
+});
+
+document.body.addEventListener("mousemove", (e) => {
+  if (isDown) {
+    x = e.clientX;
+    y = e.clientY;
+
+    $dragEl.style.left = offset.x + x + "px";
+    $dragEl.style.top = offset.y + y + "px";
+  }
+});
+
+document.addEventListener("mouseup", (e) => {
   isDown = false;
-};
-
-const drag = (event) => {
-  // console.log('mousemove', event.clientX, event.clientY);
-  if (!isDown) return;
-
-  x = event.clientX;
-  y = event.clientY;
-
-  $dragEl.style.left = x + offset.x + "px";
-  $dragEl.style.top = y + offset.y + "px";
-};
-
-function addEvent() {
-  document.body.addEventListener("mouseup", stopDrag);
-  document.body.addEventListener("mousemove", drag);
-}
-
-function init() {
-  addEvent();
-
-  // $box.addEventListener('mousedown', startDrag);
-  // $box2.addEventListener('mousedown', startDrag);
-
-  document.querySelectorAll(".drag").forEach(($box) => {
-    $box.addEventListener("mousedown", startDrag);
-  });
-}
-
-init();
+});
