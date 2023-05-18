d65d9a00b3f4ed78dc4a550dc0e06157cea0b5ec
migyeong <rmfosem613@gmail.com>
2023-05-18 17:09:02 +0900

    12

---
+++ 12/console.mjs
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+function log(msg) {
+  console.log(msg);
+}
+
+export { log };
+
+// module.exports = log;
---
+++ 12/index.mjs
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+import prettyBytes from "pretty-bytes";
+import { log } from "./console.mjs";
+
+log("12/console.mjs");
+log(prettyBytes(100));
